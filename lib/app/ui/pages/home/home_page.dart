b7a432789928419ef80de2acff2964d67973203c
Gilvan Costa <gilvancosta2025@gmail.com>
2023-10-16 10:09:21 -0300

    finishing

--- lib/app/ui/pages/home/home_page.dart
+++ lib/app/ui/pages/home/home_page.dart
@@ -3,7 +3,6 @@
 import 'package:flutter/material.dart';
 // import 'package:firebase_core/firebase_core.dart';
 
-import 'package:firebase_core/firebase_core.dart';
 import 'package:provider/provider.dart';
 
 import '../../../domain/entities/user/user_identity.dart';
